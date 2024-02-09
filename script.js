77a65fca7df74ca47f20bd14067cbe4727862572
Jesse Osborne <jesseosborne615@gmail.com>
2024-02-09 15:41:15 -0500

    added javascript, updated layout

---
+++ script.js
@@ -0,0 +1,16 @@
+function readMore(post) {
+    let dots = document.querySelector(`.card[data-post="${post}"] .dots`);
+    let moreText = document.querySelector(`.card[data-post="${post}"] .more`); 
+    let btnText = document.querySelector(`.card[data-post="${post}"] .myBtn`);
+  
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.textContent = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.textContent = "Read less"; 
+        moreText.style.display = "inline";
+    }
+  }
+  
